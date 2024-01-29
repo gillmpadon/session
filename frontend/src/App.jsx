e7ab6fbeb1973e2bbe622c9d77a70b2e36e9e268
gillmpadon <gillbertmpadon@gmail.com>
2024-01-29 23:02:30 +0800

    okayna

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 const App = () => {
-  return <div>App</div>;
+  const [name, setName] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://backend-4ak1.onrender.com/okayna")
+      .then((result) => {
+        console.log(result.data);
+        setName(result.data.message);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return <div>{name}</div>;
 };
 
 export default App;
